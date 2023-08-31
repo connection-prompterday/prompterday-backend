7bd9a8a98118a0fa4810e1f833f9758946f46be8
kimsoo0119 <illppang@naver.com>
2023-09-01 01:53:17 +0900

    Refactor(ChatGpt): 속도 향상을 위한 gpt 모델 추가, 조언/추출 호출 메서드 분리, 예외처리를 위한 필터 추가 #6

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './common/exeptions/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -22,6 +23,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(port);
 
