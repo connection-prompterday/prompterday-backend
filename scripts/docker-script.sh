32d062db5468d98020f08a963f9aaa5c0393bcf3
Kimsoo0119 <96464209+Kimsoo0119@users.noreply.github.com>
2023-09-19 15:55:15 +0900

    Update docker-script.sh

--- scripts/docker-script.sh
+++ scripts/docker-script.sh
@@ -4,6 +4,8 @@ DOCKER_IMAGE_NAME=test-img
 
 DOCKER_CONTAINER_NAME=test-container
 
-docker build -t ${DOCKER_IMAGE_NAME} ../../ # <--- 프로젝트 루트 경로에 대한 상대경로
+cd /home/ubuntu/backend
+
+docker build -t ${DOCKER_IMAGE_NAME} ./ # <--- 프로젝트 루트 경로에 대한 상대경로
 
 docker run -d -p 3001:3000 --name ${DOCKER_CONTAINER_NAME} ${DOCKER_IMAGE_NAME}
